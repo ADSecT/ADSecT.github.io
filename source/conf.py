d68e0e165fd072bbba75b8b1f7337d078f1d5910
Allenpandas <651015357@qq.com>
2023-08-21 19:46:33 +0800

    sphinx start

--- source/conf.py
+++ source/conf.py
@@ -56,9 +56,9 @@ exclude_patterns = []
 #
 
 #html_theme = 'alabaster'
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
